9db054b2a0841980c03f17b978ae27e9b630fc16
Vivian Ludrick <116781909+vivalchemy@users.noreply.github.com>
2024-09-24 23:26:58 +0530

    added a check to make sure the base dir for roadmap doesn't end in '/'

--- internal/file_utils.go
+++ internal/file_utils.go
@@ -12,13 +12,16 @@ CreateDirectory creates a directory for the subject based on its name.
 
 Example usage:
 
-	subjectDir := CreateDirectory("new-directory)
-	fmt.Printf("Directory created at: %s\n", subjectDir)
+subjectDir := CreateDirectory("new-directory")
+fmt.Printf("Directory created at: %s\n", subjectDir)
 */
 func CreateDirectory(subjectName string) string {
 	baseDir := os.Getenv("ROADMAP_HOME")
 	if baseDir == "" {
 		baseDir = "/home/shadow/Public/roadmap/tmp"
+	}
+	if baseDir[len(baseDir)-1:] == "/" {
+		baseDir = baseDir[:len(baseDir)-1]
 	}
 	subjectDir := fmt.Sprintf("%s/%s", baseDir, strings.ReplaceAll(subjectName, " ", "-"))
 	err := os.MkdirAll(subjectDir, 0755)
